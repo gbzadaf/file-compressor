b5dededab6fee1abdfca64f7c02e2e5aba720656
gbzadaf <fonsecagb23@gmail.com>
2026-05-02 16:34:36 -0300

    test main

--- src/com/compressor/Main.java
+++ src/com/compressor/Main.java
@@ -14,7 +14,13 @@ public class Main {
         Path compressed   = Path.of("output.huff");
         Path decompressed = Path.of("output.txt");
 
-        java.nio.file.Files.writeString(input, "aabbbcccc");
+        java.nio.file.Files.writeString(input,
+                "Java é uma linguagem de programação orientada a objetos. " +
+                        "Foi desenvolvida por James Gosling na Sun Microsystems. " +
+                        "É amplamente usada no desenvolvimento de aplicações web, " +
+                        "mobile, desktop e sistemas embarcados. " +
+                        "O principal lema do Java é: escreva uma vez, rode em qualquer lugar."
+        );
 
         Compressor.compress(input, compressed);
         Decompressor.decompress(compressed, decompressed);
@@ -22,11 +28,12 @@ public class Main {
         String original     = java.nio.file.Files.readString(input);
         String reconstructed = java.nio.file.Files.readString(decompressed);
 
-        System.out.println("Original:      " + original);
-        System.out.println("Reconstruído:  " + reconstructed);
         System.out.println("Idênticos: " + original.equals(reconstructed));
-
 
     }
 
 }
+
+/*Nesse caso o arquivo compactado ficou maior que o original, isso acontece porque o cabeçalho com a tabela de frequências
+ tem um custo fixo alto para textos pequenos. O cenário muda para textos de LIVROS, LOGS DE SISTEMA, valendo a pena a
+ compactação.*/ 
